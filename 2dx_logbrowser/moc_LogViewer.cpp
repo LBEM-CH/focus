4f6fb955ba1d3d64817dff9ec4c705b46296be7d
marheit <marheit@cb738334-da44-0410-bf9e-9ace271e2f5b>
2010-01-20 15:56:35 +0000

    generated files should not be in the repository
    git-svn-id: svn+ssh://svncina.ethz.ch/repos/2dx/trunk@18 cb738334-da44-0410-bf9e-9ace271e2f5b

--- 2dx_logbrowser/moc_LogViewer.cpp
+++ 2dx_logbrowser/moc_LogViewer.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'LogViewer.h'
 **
-** Created: Thu Oct 16 09:54:33 2008
-**      by: The Qt Meta Object Compiler version 59 (Qt 4.4.0)
+** Created: Wed Jan 20 16:56:02 2010
+**      by: The Qt Meta Object Compiler version 61 (Qt 4.5.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,8 +10,8 @@
 #include "LogViewer.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'LogViewer.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 59
-#error "This file was generated using the moc from 4.4.0. It"
+#elif Q_MOC_OUTPUT_REVISION != 61
+#error "This file was generated using the moc from 4.5.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,12 +20,13 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_LogViewer[] = {
 
  // content:
-       1,       // revision
+       2,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   10, // methods
+       1,   12, // methods
        0,    0, // properties
        0,    0, // enums/sets
+       0,    0, // constructors
 
  // slots: signature, parameters, type, tag, flags
       11,   10,   10,   10, 0x0a,
@@ -51,7 +52,7 @@ void *LogViewer::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
     if (!strcmp(_clname, qt_meta_stringdata_LogViewer))
-	return static_cast<void*>(const_cast< LogViewer*>(this));
+        return static_cast<void*>(const_cast< LogViewer*>(this));
     return QWidget::qt_metacast(_clname);
 }
 
@@ -63,6 +64,7 @@ int LogViewer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
         case 0: updateViewer(); break;
+        default: ;
         }
         _id -= 1;
     }
--- 2dx_logbrowser/moc_ViewerContainer.cpp
+++ 2dx_logbrowser/moc_ViewerContainer.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'ViewerContainer.h'
 **
-** Created: Thu Oct 16 09:54:34 2008
-**      by: The Qt Meta Object Compiler version 59 (Qt 4.4.0)
+** Created: Wed Jan 20 16:56:03 2010
+**      by: The Qt Meta Object Compiler version 61 (Qt 4.5.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,8 +10,8 @@
 #include "ViewerContainer.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'ViewerContainer.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 59
-#error "This file was generated using the moc from 4.4.0. It"
+#elif Q_MOC_OUTPUT_REVISION != 61
+#error "This file was generated using the moc from 4.5.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,12 +20,13 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_ViewerContainer[] = {
 
  // content:
-       1,       // revision
+       2,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   10, // methods
+       3,   12, // methods
        0,    0, // properties
        0,    0, // enums/sets
+       0,    0, // constructors
 
  // slots: signature, parameters, type, tag, flags
       28,   17,   16,   16, 0x0a,
@@ -55,7 +56,7 @@ void *ViewerContainer::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
     if (!strcmp(_clname, qt_meta_stringdata_ViewerContainer))
-	return static_cast<void*>(const_cast< ViewerContainer*>(this));
+        return static_cast<void*>(const_cast< ViewerContainer*>(this));
     return QTabWidget::qt_metacast(_clname);
 }
 
@@ -69,6 +70,7 @@ int ViewerContainer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         case 0: changeVerbosityLevel((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 1: updateLogViewers(); break;
         case 2: updateSelectedViewer((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
         }
         _id -= 3;
     }

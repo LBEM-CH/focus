3afe2a3df1a864b44f0feb17f9d86df61e49c0f3
marheit <marheit@cb738334-da44-0410-bf9e-9ace271e2f5b>
2010-08-17 12:41:49 +0000

    to change the linkage of the plugins
    git-svn-id: svn+ssh://svncina.ethz.ch/repos/2dx/trunk@485 cb738334-da44-0410-bf9e-9ace271e2f5b

---
+++ installation/mac_osx_installer/change_dylibs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# change_dylibs
+build_dir=`pwd`
+apps="2dx_image/2dx_image.app/Contents/PlugIns/imageformats 2dx_merge/2dx_merge.app/Contents/PlugIns/imageformats"
+for loop in $apps
+do
+	cd $loop
+	echo "in $loop" 
+	for loop in `ls`
+	do
+		echo "changing the dylibs of" $loop
+		install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $loop 	
+		install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $loop
+		#if test "$loop" -eq "libqsvg.dylib" ; then
+		#	echo "additional dylibs have to be changed for $loop"
+			install_name_tool -change QtSvg.framework/Versions/4/QtSvg @executable_path/../Frameworks/QtSvg.framework/Versions/4/QtSvg $loop 	
+			install_name_tool -change QtXml.framework/Versions/4/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml $loop
+		#fi
+		otool -L $loop
+	done
+	# chnage back to build dir
+	cd $build_dir
+done

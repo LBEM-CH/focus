1813f1499314954278fdd80102fabc53159ce037
marheit <marheit@cb738334-da44-0410-bf9e-9ace271e2f5b>
2010-11-15 13:51:09 +0000

    the current project dir is now stored in the new config file 2dx_merge-user.cfg and used when selecting the project
    git-svn-id: svn+ssh://svncina.ethz.ch/repos/2dx/trunk@759 cb738334-da44-0410-bf9e-9ace271e2f5b

--- 2dx_merge/main.cpp
+++ 2dx_merge/main.cpp
@@ -5,7 +5,7 @@
 #include <QDebug>
 #include "mainWindow.h"
 
-void initializeProject(const QString &workingDir)
+QString getAppDir()
 {
   QString appDir = QApplication::applicationDirPath();
   QString sep = "/../";
@@ -19,37 +19,30 @@ void initializeProject(const QString &workingDir)
     sep+="../";
     tries++;
   }
-  if(!QFileInfo(appDir + sep + "config/2dx_master.cfg").exists()) exit(0);
+  if(QFileInfo(appDir + sep + "config/2dx_master.cfg").exists())
+  {
+    return QString(appDir + sep);
+  }
+  else
+    return QString();
   
+}
+void initializeProject(const QString &appDir, const QString &workingDir)
+{
+ 
   QDir dir(workingDir);
   dir.mkdir("merge");
   dir.mkpath("merge/proc");
   dir.mkpath("merge/LOGS");
-  confData data(workingDir + "/merge/" + "2dx_merge.cfg", appDir + sep + "config/2dx_master.cfg");
-  //confData data(workingDir +"/2dx_master.cfg", appDir + sep + "config/2dx_master.cfg");
+  confData data(workingDir + "/merge/" + "2dx_merge.cfg", appDir + "config/2dx_master.cfg");
   data.setSymLink("merge/2dx_merge.cfg", workingDir +"/2dx_master.cfg");
   data.save();
 }
 
-void reviseProject(const QString &workingDir)
+void reviseProject(const QString &appDir, const QString &workingDir)
 {
-  QString appDir = QApplication::applicationDirPath();
-  QString sep = "/../";
-  #ifdef Q_WS_MAC
-    appDir+="/../../../";
-  #endif
-  int tries = 0;
-  while(!QFileInfo(appDir + sep + "config/2dx_master.cfg").exists() && tries<3)
-  {
-    qDebug()<<(appDir + sep + "config/2dx_master.cfg")<<" does not exist!";
-    sep+="../";
-    tries++;
-  }
-  if(!QFileInfo(appDir + sep + "config/2dx_master.cfg").exists()) exit(0);
-  
   QDir dir(workingDir);
-  //to enter new variables from the 2dx_master.cfg
-  confData data(workingDir + "/merge/" + "2dx_merge.cfg", appDir + sep + "config/2dx_master.cfg");
+  confData data(workingDir + "/merge/" + "2dx_merge.cfg", appDir + "config/2dx_master.cfg");
   data.save();
 }
 
@@ -58,21 +51,38 @@ int main(int argc, char **argv)
   QApplication app(argc,argv);
   app.setApplicationName("2dx_merge");
 
-  QString workingDir = QFileDialog::getExistingDirectory(NULL,"Select a Project Directory",QDir::homePath());
-  if(workingDir.isEmpty() || !QDir(workingDir).exists()) exit(0);
+  QString appDir = getAppDir();
+  if(appDir.isEmpty()) exit(0);
 
+  QString configDir = appDir + "/config";
+
+  // read user config to get the latest image dir
+  confData* userData = new confData(QDir::homePath() + "/.2dx/" + "2dx_merge-user.cfg", configDir + "/" + "2dx_merge-user.cfg");
+  userData->save();
+  QString userDirPath = userData->get("workingDir","value");
+  qDebug() << "The last used working dir is: " << userDirPath;
+  if(userDirPath.isEmpty()) 
+      userDirPath = QDir::homePath(); 
+ 
+  
+  QString workingDir = QFileDialog::getExistingDirectory(NULL,"Select a Project Directory",userDirPath);
+  if(workingDir.isEmpty() || !QDir(workingDir).exists()) exit(0);
+  //save the selected dir as working dir in 2dx_merg-user.cfg
+  userData->set("workingDir", workingDir);
+  userData->save();
+ 
   if(!QFileInfo(workingDir +"/merge/" + "2dx_merge.cfg").exists())
   {
     quint32 choice = QMessageBox::question(NULL,"Confirm Create new Project?","No configuration data found in:\n"+workingDir+"\n\nCreate new config files in this directory?","Create","Cancel",QString(),0,1);
     if(choice) return 0;
     else
     {
-      initializeProject(workingDir);
+      initializeProject(appDir, workingDir);
     }
   }
   else
   {
-    reviseProject(workingDir);
+    reviseProject(appDir, workingDir);
   }
 
 

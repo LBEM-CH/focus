e72fc27514b95f72316ddbcb11b172b7486ba324
marheit <marheit@cb738334-da44-0410-bf9e-9ace271e2f5b>
2011-01-21 22:56:34 +0000

    nightly build is now using the installer_nightly_build.sh script to create the mac installer packages
    git-svn-id: svn+ssh://svncina.ethz.ch/repos/2dx/trunk@871 cb738334-da44-0410-bf9e-9ace271e2f5b

---
+++ installation/mac_osx_installer/installer_nightly_build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# scirpt to package the 2dx installer with package maker 
+#
+# Marcel Arheit  21.01.2011
+
+if [ $# -lt 2 ]
+then
+	echo "Not enough arguments specified"
+	echo "Usage: `basename $0:` <root_dir> <out_file>" >&2
+	echo 'Aborting!'
+	exit 1
+fi
+DIR=`dirname $0`
+ROOT=$1
+OUTFILE=$2
+/Developer/usr/bin/packagemaker \
+--title "2dx Nightly Build" \
+--version  DATE=`date "+%d_%b_%Y"` \
+--filter "\.DS_Store" \
+--resources $DIR/Resources/ \
+--scripts $DIR/Resources/ \
+--root-volume-only \
+--domain system \
+--verbose \
+--no-relocate \
+-l "/usr/local/2dx" \
+--target 10.5 \
+--id org.2dx.pkg \
+--root $ROOT \
+--out $OUTFILE

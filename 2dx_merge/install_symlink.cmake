a42a7eab210cd14f8f910a4047d9c6365bbac7f4
marheit <marheit@cb738334-da44-0410-bf9e-9ace271e2f5b>
2011-10-31 11:02:24 +0000

    added additional cmake files
    git-svn-id: svn+ssh://svncina.ethz.ch/repos/2dx/trunk@1421 cb738334-da44-0410-bf9e-9ace271e2f5b

---
+++ 2dx_merge/install_symlink.cmake
@@ -0,0 +1,16 @@
+############################################################################
+# A 2dx CMake install script                                               #
+############################################################################
+# Creates the necessary symbolic links to the kernel                       #
+# The author was:                                                          #
+# 2011 Marcel Arheit                                                       #
+############################################################################
+#
+cmake_minimum_required(VERSION 2.6)
+if(CMAKE_HOST_UNIX)
+	execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ../kernel/2dx_merge/scripts-standard ${CMAKE_INSTALL_PREFIX}/2dx_merge/scripts-standard)
+	execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ../kernel/2dx_merge/scripts-custom ${CMAKE_INSTALL_PREFIX}/2dx_merge/scripts-custom)
+	execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ../kernel/mrc/bin ${CMAKE_INSTALL_PREFIX}/2dx_merge/bin)
+endif(CMAKE_HOST_UNIX)
+
+

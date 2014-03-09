3eafd864e7f02b425c49b4605f953f40cd9f527b
Sebastian Scherer <sebastian.scherer@unibas.ch>
2014-03-09 22:03:10 +0100

    FFTs of all frames now calculated

---
+++ kernel/mrc/source/movie_mode_py/disable_movie.py
@@ -0,0 +1,27 @@
+from EMAN2 import *
+from sparx import *
+
+import os
+
+
+if __name__ == "__main__":
+	
+	if len(sys.argv) != 2:
+		sys.exit("Missuseage detected")
+
+	frame_folder = sys.argv[1]
+	
+	f = open(frame_folder + "2dx_image.cfg", 'r')
+	lines = []
+	
+	for l in f:
+		if l.startswith("set movie_enable"):
+			lines.append('set movie_enable = "n"\n')
+		else:
+			lines.append(l)
+	f.close()
+	
+	fout = open(frame_folder + "2dx_image.cfg", 'w')
+	
+	fout.writelines(lines)
+	fout.close()

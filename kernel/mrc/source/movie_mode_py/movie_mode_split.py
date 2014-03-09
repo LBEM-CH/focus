9c7955b4a9712033964b008fcf7cb8019ca36429
Sebastian Scherer <sebastian.scherer@unibas.ch>
2014-03-09 17:10:14 +0100

    splitting into frames in InitFilesAndParams works now

--- kernel/mrc/source/movie_mode_py/movie_mode_split.py
+++ kernel/mrc/source/movie_mode_py/movie_mode_split.py
@@ -1,12 +1,35 @@
 from EMAN2 import *
 from sparx import *
 
+import os
+
 
 if __name__ == "__main__":
 	
-	if len(sys.argv) != 2:
+	if len(sys.argv) != 3:
 		sys.exit("Missuseage detected")
 
 	stack_file = sys.argv[1]
+	image_name = sys.argv[2]
 	
-	print "Doing splitting for: ", stack_file
+	
+
+	stack = get_image(stack_file)
+	nx = stack.get_xsize()
+	ny = stack.get_ysize()
+	nz = stack.get_zsize()
+	
+	print "Doing splitting for:", stack_file
+	print "nz =", nz
+	
+	for i in range(0,nz):		
+		folder_name = "frames/frame_" + str(i+1)
+		output_name = folder_name + "/" + image_name + "_" + str(i+1) + ".mrc"
+		
+		print "Slitting frame", i+1, "into", output_name
+		
+		os.mkdir(folder_name)   
+		image = stack.get_clip(Region(0,0,i,nx,ny,1))
+		image.write_image(output_name)
+	
+	

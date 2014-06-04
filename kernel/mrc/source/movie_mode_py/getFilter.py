de8a3234c6c08b1257c75322129e5ce3741cd7fb
Sebastian Scherer <sebastian.scherer@unibas.ch>
2014-06-04 13:53:42 +0200

    movie-mode: status bar and filter params

--- kernel/mrc/source/movie_mode_py/getFilter.py
+++ kernel/mrc/source/movie_mode_py/getFilter.py
@@ -3,22 +3,28 @@ from sparx import *
 
 import os
 
-def w(x):
-	#return 0.5
-	return 0.7559*exp(-0.06881*x)
-	#return 0.7559*exp(-0.04881*x)
+def w(x, t, a, b):
+	if t==0:
+		return a*exp(b*x)
+	else:
+		return 0.5
 
-def getCutoff(n):
-	#return 0.5
-	return min(0.5, w(n))
+def getCutoff(n, t, a, b):
+	return min(0.5, w(n, t, a, b))
 
 if __name__ == "__main__":
 	
-	if len(sys.argv) != 2:
+	# Usage getFilter.py n type a b
+	
+	if len(sys.argv) != 5:
 		sys.exit("Missuseage detected filter")
 
 	number = int(sys.argv[1])
+	ftype = int(sys.argv[2])
+	a = float(sys.argv[3])
+	b = float(sys.argv[4])
 	
-	print getCutoff(number)
+	
+	print getCutoff(number, ftype, a, b)
 
 	

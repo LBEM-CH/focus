8589d1db03986415ccc229740e81a4873e7ac5a7
nikhilbiyani <nikhilbiyani@gmail.com>
2015-09-15 11:41:54 +0200

    First initiation of seperating 2d and 3d merge

--- include/scriptModule.h
+++ include/scriptModule.h
@@ -26,7 +26,7 @@ class scriptModule : public QWidget
   Q_OBJECT
 
   public:
-  enum moduleType {standard, custom, singleparticle};
+  enum moduleType {standard, custom, singleparticle, merge2D, merge3D};
 
   public slots:
   void execute(bool run);

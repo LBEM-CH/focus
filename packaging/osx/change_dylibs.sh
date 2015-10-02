f18aacc5eb58c92d0a1c51d61cefba452a6b29d4
nikhilbiyani <nikhilbiyani@gmail.com>
2015-10-02 14:44:50 +0200

    OSC packaging bugfix

--- packaging/osx/change_dylibs.sh
+++ packaging/osx/change_dylibs.sh
@@ -5,24 +5,24 @@
 #
 # Marcel Arheit
 # 
-# Adapted by Sebastian Scherer
+# Adapted by Sebastian Scherer, Nikhil Biyani
 #
 #
 # How to add antoher library 
 # 1. Add if statement 
-# 2. Copy Lib to build_dir/loop
+# 2. Copy Lib to install_dir/loop
 # 3. install_name_tool for Lib
 
 
 if [ $# -lt 1 ]
 then
 	echo "No build directory specified"
-	echo "Usage: `basename $0:` <build_dir>" >&2
+	echo "Usage: `basename $0:` <install_dir>" >&2
 	echo 'Aborting!'
 	exit 1
 fi
 
-build_dir=$1
+install_dir=$1
 echo "the build dir is $1"
 if [ -f /opt/local/lib/libfftw3.3.dylib ]; then
 	FFTW_LIB=/opt/local/lib/libfftw3.3.dylib
@@ -162,54 +162,31 @@ fi
 binaries="kernel/mrc/lib"
 for loop in $binaries
 do
-    echo "cp $FFTW_LIB  $build_dir/$loop"
-    cp $FFTW_LIB $build_dir/$loop
-    echo "cp $FFTWF_LIB  $build_dir/$loop"
-    cp $FFTWF_LIB $build_dir/$loop
-    echo "cp $FFTW_LIB_THREAD  $build_dir/$loop"
-    cp $FFTW_LIB_THREAD $build_dir/$loop
-    echo "cp $FFTWF_LIB_THREAD  $build_dir/$loop"
-    cp $FFTWF_LIB_THREAD $build_dir/$loop	
-    echo "cp $GFORTRAN_LIB $build_dir/$loop"
-    cp $GFORTRAN_LIB $build_dir/$loop
-    echo "cp $QUADMATH_LIB $build_dir/$loop"
-    cp $QUADMATH_LIB $build_dir/$loop
-    echo "cp $CPP_LIB $build_dir/$loop"
-    cp $CPP_LIB $build_dir/$loop
-    echo "cp $GCC_LIB $build_dir/$loop"
-    cp $GCC_LIB $build_dir/$loop
-    echo "cp $OMP_LIB $build_dir/$loop"
-    cp $OMP_LIB $build_dir/$loop
-    echo "cp $SYS_LIB $build_dir/$loop"
-    cp $SYS_LIB $build_dir/$loop
+    echo "cp $FFTW_LIB  $install_dir/$loop"
+    cp $FFTW_LIB $install_dir/$loop
+    echo "cp $FFTWF_LIB  $install_dir/$loop"
+    cp $FFTWF_LIB $install_dir/$loop
+    echo "cp $FFTW_LIB_THREAD  $install_dir/$loop"
+    cp $FFTW_LIB_THREAD $install_dir/$loop
+    echo "cp $FFTWF_LIB_THREAD  $install_dir/$loop"
+    cp $FFTWF_LIB_THREAD $install_dir/$loop	
+    echo "cp $GFORTRAN_LIB $install_dir/$loop"
+    cp $GFORTRAN_LIB $install_dir/$loop
+    echo "cp $QUADMATH_LIB $install_dir/$loop"
+    cp $QUADMATH_LIB $install_dir/$loop
+    echo "cp $CPP_LIB $install_dir/$loop"
+    cp $CPP_LIB $install_dir/$loop
+    echo "cp $GCC_LIB $install_dir/$loop"
+    cp $GCC_LIB $install_dir/$loop
+    echo "cp $OMP_LIB $install_dir/$loop"
+    cp $OMP_LIB $install_dir/$loop
+    echo "cp $SYS_LIB $install_dir/$loop"
+    cp $SYS_LIB $install_dir/$loop
 done
 
-#mkdir $build_dir/kernel/mrc/lib/system/
-#for sys_lib in `ls /usr/lib/system/*.dylib`
-#do
-#cp $sys_lib $build_dir/kernel/mrc/lib/system/
-#done
-
-#for loop in `ls /usr/lib/system/`
-#do
-#echo $loop
-#echo "install_name_tool -change /usr/lib/system/$loop @executable_path/../lib/system/$loop $build_dir/kernel/mrc/lib/libSystem.B.dylib"
-#install_name_tool -change /usr/lib/system/$loop @executable_path/../lib/system/$loop $build_dir/kernel/mrc/lib/libSystem.B.dylib
-#done
-
-#for loop1 in `ls /usr/lib/system/`
-#do
-#    for loop2 in `ls /usr/lib/system/`
-#    do
-#        echo "install_name_tool -change /usr/lib/system/$loop2 @executable_path/../lib/system/$loop2 $build_dir/kernel/mrc/lib/system/$loop1"
-#        install_name_tool -change /usr/lib/system/$loop2 @executable_path/../lib/system/$loop2 $build_dir/kernel/mrc/lib/system/$loop1
-#    done
-#    otool -L $build_dir/kernel/mrc/lib/system/$loop1
-#done
-
 fortran_bin="kernel/mrc/bin"
-path="$build_dir/$fortran_bin"
-lib_path="$build_dir/kernel/mrc/lib"
+path="$install_dir/$fortran_bin"
+lib_path="$install_dir/kernel/mrc/lib"
 target_lib_path="/opt/2dx/kernel/mrc/lib"
 
 
@@ -236,7 +213,7 @@ do
 	otool -L $file 
 done
 
-for loop in $build_dir/$binaries/*.dylib
+for loop in $install_dir/$binaries/*.dylib
 do
 install_name_tool -change $FFTW_LIB @executable_path/../lib/libfftw3.3.dylib $loop
 install_name_tool -change $FFTWF_LIB @executable_path/../lib/libfftw3f.3.dylib $loop
@@ -250,47 +227,3 @@ install_name_tool -change $OMP_LIB @executable_path/../lib/libgomp.1.dylib  $loo
 install_name_tool -change $SYS_LIB @executable_path/../lib/libSystem.B.dylib  $loop
 otool -L $loop
 done
-
-#apps="2dx_image/2dx_image.app/Contents/PlugIns/imageformats 2dx_merge/2dx_merge.app/Contents/PlugIns/imageformats"
-#for loop in $apps
-#do
-#        path=$build_dir"/"$loop	
-#	echo "in $path"
-#	for loop in `ls $path`
-#	do
-#		file=$path"/"$loop
-#		echo "changing the dylibs of" $loop
-#		echo "with absolute path" $file
-#		install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $file 	
-#		install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $file 
-#		install_name_tool -change $CPP_LIB @executable_path/../MacOS/libstdc++.6.dylib $file  
-#		install_name_tool -change $GCC_LIB @executable_path/../MacOS/libgcc_s.1.dylib $file  
-#		otool -L $file
-#		if [ $loop = "libqsvg.dylib" ] ; then
-#			echo "removing $file"
-#			rm $file
-#		fi
-#	done
-#done
-
-
-#executables="2dx_image/2dx_image.app/Contents/MacOS/2dx_image 2dx_merge/2dx_merge.app/Contents/MacOS/2dx_merge"
-#for exe in $executables 
-#do
-#	file="$build_dir/$exe"
-#	echo "changing the dylibs of $file"
-#	install_name_tool -change $FFTW_LIB @executable_path/libfftw3f.3.dylib $file
-#	install_name_tool -change $FFTW_LIB_THREAD @executable_path/libfftw3f_threads.3.dylib $file
-#	install_name_tool -change $QUADMATH_LIB @executable_path/libquadmath.0.dylib  $file 
-#	install_name_tool -change $GFORTRAN_LIB  @executable_path/libgfortran.3.dylib $file 
-#	install_name_tool -change $QUADMATH_LIB  @executable_path/libquadmath.0.dylib $gfortran.3.dylib
-#	otool -L $file 
-#done
-#
-#lib_path="$build_dir/2dx_image/2dx_image.app/Contents/MacOS"
-#echo "install_name_tool -change $QUADMATH_LIB  $target_lib_path/libquadmath.0.dylib $lib_path/libgfortran.3.dylib"
-#install_name_tool -change $QUADMATH_LIB  $target_lib_path/libquadmath.0.dylib $lib_path/libgfortran.3.dylib
-#
-#lib_path="$build_dir/2dx_merge/2dx_merge.app/Contents/MacOS"
-#echo "install_name_tool -change $QUADMATH_LIB  $target_lib_path/libquadmath.0.dylib $lib_path/libgfortran.3.dylib"
-#install_name_tool -change $QUADMATH_LIB  $target_lib_path/libquadmath.0.dylib $lib_path/libgfortran.3.dylib

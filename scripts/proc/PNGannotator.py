cdd58556cc5dda5ae576e4dfd198819888a16c9a
sthennin <henning.stahlberg@unibas.ch>
2016-10-27 21:48:00 +0200

    PNGannotator.py now blurs image

--- scripts/proc/PNGannotator.py
+++ scripts/proc/PNGannotator.py
@@ -1,24 +1,28 @@
 import os,sys
 import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 if __name__ == "__main__":
-	
-	if len(sys.argv) != 6:
-		sys.exit("Usage: PNGannotator.py <Input-Image> <Output-Image> <posx> <posy> <Text> ")
 
-	infile = sys.argv[1]
-	outfile = sys.argv[2]
-	posx = int(sys.argv[3])
-	posy = int(sys.argv[4])
-	text1 = sys.argv[5]
-			
-	image = Image.open(infile)
-	
-	draw  = ImageDraw.Draw(image)
-	# font  = ImageFont.truetype("arial.ttf", 20, encoding="unic")
+        if len(sys.argv) != 7:
+                sys.exit("Usage: PNGannotator.py <Input-Image> <Output-Image> <posx> <posy> <doblur: 0|1> <Text> ")
 
-	draw.text( (posx,posy), text1, fill='#ffffa0')
+        infile = sys.argv[1]
+        outfile = sys.argv[2]
+        posx = int(sys.argv[3])
+        posy = int(sys.argv[4])
+        doblur = int(sys.argv[5])
+        text1 = sys.argv[6]
 
-	image.save(outfile)
-		
+        image = Image.open(infile)
+  
+        if doblur == 1:
+                image = image.filter(ImageFilter.GaussianBlur(radius=10))
+
+        draw  = ImageDraw.Draw(image)
+        # font  = ImageFont.truetype("arial.ttf", 20, encoding="unic")
+
+        draw.text( (posx,posy), text1, fill='#ffffa0')
+
+        image.save(outfile)
+

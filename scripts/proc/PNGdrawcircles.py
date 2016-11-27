7ce7feca762bd4482e4e5081fd69511d507ec997
CINA <cina@bs-zorro01.ethz.ch>
2016-11-27 11:44:27 +0100

    PNGdrawcircles.py fixed. But non-square images are not correctly produced by fMrcConverter

--- scripts/proc/PNGdrawcircles.py
+++ scripts/proc/PNGdrawcircles.py
@@ -9,22 +9,24 @@ if __name__ == "__main__":
 
         infilename = sys.argv[1]
         outfilename = sys.argv[2]
-        starfilename = int(sys.argv[3])
-        diameter = int(sys.argv[4])
+        starfilename = sys.argv[3]
+        diameter = float(sys.argv[4])
 
         image = Image.open(infilename)
 
+	width,height = image.size
+
         starfile = open(starfilename, 'r')
   
-        for i in range (0,8):
+        for i in range (0,9):
                 starfile.readline()
 
         xpos=[]; ypos=[]; ccva=[];
 
         for line in starfile:
                 data_split = line.split()
-                xpos.append(float(data_split[0]))
-                ypos.append(float(data_split[1]))
+                xpos.append(        float(data_split[0]) )
+                ypos.append((height-float(data_split[1])))
                 ccva.append(float(data_split[4]))
 
         ccmax = 0.0
@@ -42,21 +44,25 @@ if __name__ == "__main__":
         print "Found ",len(xpos)," particle positions."
         print "CCvalues range from ",ccmin," to ",ccmax,"."
 
+	draw = ImageDraw.Draw(image)
+
         for i in range(0,len(xpos)):
 
-                x1 = xpos[i] - diameter / 2.0
-                x2 = xpos[i] + diameter / 2.0
-                y1 = ypos[i] - diameter / 2.0
-                y2 = ypos[i] + diameter / 2.0
+                cr = int(( (ccva[i]-ccmin) / ccdiff ) * 128 + 128)
+                cg = int(( (ccva[i]-ccmin) / ccdiff ) * 128 + 128)
+                cb = int(128)
+                ct = int(255)
 
-                cr = ( (ccva[i]-ccmin) / ccdiff ) * 128 + 128
-                cg = ( (ccva[i]-ccmin) / ccdiff ) * 128 + 128
-                cb = 0.0
-                ct = 255
+		for n in range (0,20):
+	                x1 = xpos[i] - (diameter / 2.0 - n/2.0)
+   	             	x2 = xpos[i] + (diameter / 2.0 - n/2.0)
+  	              	y1 = ypos[i] - (diameter / 2.0 - n/2.0)
+  	              	y2 = ypos[i] + (diameter / 2.0 - n/2.0)
+                	draw.ellipse((x1,y1,x2,y2),fill=None,outline=(cr,cg,cb,ct))
+        
+	del draw
 
-                PIL.ImageDraw.Draw.ellipse(x1,y1,x2,y2,fill=None,outline=(cr,cg,cb,ct))
-
-        image.save(outfilename)
+	image.save(outfilename)
 
         starfile.close()
 

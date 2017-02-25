2b1c58ddac252dd244268720c16d3bc95cb82136
Nikhil Biyani <nikhilbiyani@users.noreply.github.com>
2017-02-25 11:07:46 +0100

    Update analyticstracking.php

--- web/status/bin/analyticstracking.php
+++ web/status/bin/analyticstracking.php
@@ -4,7 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-74873514-2', 'auto');
+  ga('create', 'UA-xxxxxxxx-x', 'auto');
   ga('send', 'pageview');
 
 </script>

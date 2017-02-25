4ebfd2e96fdb96ad1b9660b3983ef82b6a204a3b
Nikhil Biyani <nikhilbiyani@gmail.com>
2017-02-25 11:27:58 +0100

    Updated tracking details on status webpage

--- web/status/status.php
+++ web/status/status.php
@@ -51,6 +51,16 @@ echo '
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="blue">
 
+        <script>
+            (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');
+
+            ga(\'create\', \''. $configs->analytics_tracking_id . '\', \'auto\');
+            ga(\'send\', \'pageview\');
+        </script>
+
     </head>
 ';
 

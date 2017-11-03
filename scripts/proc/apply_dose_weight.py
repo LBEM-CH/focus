b283091f3bb0815d5c20ae037854c23d314731e2
rdrighetto <ricardorighetto@gmail.com>
2017-11-03 18:06:22 +0100

    Implemented dose-weighting filtering script to generated an aligned average of frames up to desired total dose

---
+++ scripts/proc/apply_dose_weight.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import focus_utilities as util
+import ioMRC
+import sys
+
+def main():
+
+	stack = ioMRC.readMRC( sys.argv[1] )[0]
+	output = sys.argv[2]
+	apix = float( sys.argv[3] )
+	frame_dose = float( sys.argv[4] )
+	pre_dose = float( sys.argv[5] )
+	total_dose = float( sys.argv[6] )
+
+	dw_avg = util.FilterDoseWeight( stack, apix=apix, frame_dose=frame_dose, pre_dose=pre_dose, total_dose=total_dose )
+
+	ioMRC.writeMRC( dw_avg, output, dtype='float32', pixelsize=apix, quickStats=False )
+
+
+if __name__ == "__main__":
+	main()

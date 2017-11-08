442d499b6f79601a25ab87110a49643629c89dfb
rdrighetto <ricardorighetto@gmail.com>
2017-11-08 14:24:49 +0100

    Dose-weighting filter now takes kV into account as well (but probably only valid in the range 200-300 kV)

--- scripts/proc/apply_dose_filter.py
+++ scripts/proc/apply_dose_filter.py
@@ -12,8 +12,9 @@ def main():
 	frame_dose = float( sys.argv[4] )
 	pre_dose = float( sys.argv[5] )
 	total_dose = float( sys.argv[6] )
+	kv = float( sys.argv[7] )
 
-	dw_avg = util.FilterDoseWeight( stack, apix=apix, frame_dose=frame_dose, pre_dose=pre_dose, total_dose=total_dose )
+	dw_avg = util.FilterDoseWeight( stack, apix=apix, frame_dose=frame_dose, pre_dose=pre_dose, total_dose=total_dose, kv=kv )
 
 	ioMRC.writeMRC( dw_avg, output, dtype='float32', pixelsize=apix, quickStats=False )
 

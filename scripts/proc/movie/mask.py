9bd76a7a94684d16359c506526c4d7bf3c87b034
rdrighetto <ricardorighetto@gmail.com>
2019-09-05 15:45:43 +0200

    fixed scripts/proc/movie/mask.py

--- scripts/proc/movie/mask.py
+++ scripts/proc/movie/mask.py
@@ -20,32 +20,32 @@ if __name__ == "__main__":
 	maskinginfo = sys.argv[3]
 	
 	frame = get_image(filename_in)
-        frameinfo = info(frame)
-        framesize = frameinfo[4]
-        print "Read image ", filename_in, ", size = ", framesize
+	frameinfo = info(frame)
+	framesize = frameinfo[4]
+	print "Read image ", filename_in, ", size = ", framesize
 
-        maskfile = get_image(maskinginfo)
-        maskinfo = info(maskfile)
-        masksize = maskinfo[4]
-        print "Read image ", maskinginfo, ", size = ", masksize
+	maskfile = get_image(maskinginfo)
+	maskinfo = info(maskfile)
+	masksize = maskinfo[4]
+	print "Read image ", maskinginfo, ", size = ", masksize
 
-        r = 1.0 * framesize / masksize
-        print "Upscaling by a factor of %10.6f" % r
+	r = 1.0 * framesize / masksize
+	print "Upscaling by a factor of %10.6f" % r
 
-        mask = resample(maskfile,r)
-        maskinfo = info(mask)
-        maskmax  = maskinfo[3]
-        masksize = maskinfo[4]
-        print "New masking file ", maskinginfo, " has size = ", masksize
+	mask = resample(maskfile,r)
+	maskinfo = info(mask)
+	maskmax  = maskinfo[3]
+	masksize = maskinfo[4]
+	print "New masking file ", maskinginfo, " has size = ", masksize
 	if maskmax < 1.0:
 		maskmax = 1.0
 
-        s = info(frame,mask)
-        average = s[0]
+	s = info(frame,mask)
+	average = s[0]
 
-        frame -= average
-        frame *= mask / maskmax
-        frame += average
+	frame -= average
+	frame *= mask / maskmax
+	frame += average
 
 	frame.write_image(filename_out)
 			

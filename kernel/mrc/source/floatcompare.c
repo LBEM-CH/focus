06859c68872e6a63040a13aac37853faf7b23889
Konstantin Weindel <konstantin.weindel@epfl.ch>
2025-05-13 14:50:28 +0200

    fix: correct main function declaration and improve formatting for readability

--- kernel/mrc/source/floatcompare.c
+++ kernel/mrc/source/floatcompare.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	float a,b;
-	sscanf(argv[1],"%f",&a);
-	sscanf(argv[2],"%f",&b);
-	if(a>b)
+	float a, b;
+	sscanf(argv[1], "%f", &a);
+	sscanf(argv[2], "%f", &b);
+	if (a > b)
 		printf("1");
 	else
 		printf("0");
